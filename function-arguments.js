bada495620b04ec65267d80ec336325dd4339da4
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-03 22:20:36 +0000

    Basic understanding of Objects and Functions

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+const math = function(operandOne, operandTwo, operandThree){
+  return ((operandTwo * operandThree) + operandOne);
+}
+
+console.log(math(53,61,67));
---
+++ object-keys.js
@@ -0,0 +1,9 @@
+const car = {  
+  make: 'Honda',  
+  model: 'Accord',  
+  year: 2020  
+}
+
+const carKeys = Object.keys(car);
+
+console.log(carKeys);
